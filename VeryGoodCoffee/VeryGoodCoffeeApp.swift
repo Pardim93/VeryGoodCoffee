903104147fe57726322ffa2a02a854e517efc910
wellington ferreira <wellpardim10@gmail.com>
2025-11-24 03:22:58 -0800

    Initial Commit

---
+++ VeryGoodCoffee/VeryGoodCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeryGoodCoffeeApp.swift
+//  VeryGoodCoffee
+//
+//  Created by wellington ferreira on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct VeryGoodCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VeryGoodCoffeeTests/VeryGoodCoffeeTests.swift
@@ -0,0 +1,17 @@
+//
+//  VeryGoodCoffeeTests.swift
+//  VeryGoodCoffeeTests
+//
+//  Created by wellington ferreira on 24/11/25.
+//
+
+import Testing
+@testable import VeryGoodCoffee
+
+struct VeryGoodCoffeeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
